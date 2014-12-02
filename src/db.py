9a3a90e62df7416ad15a3823ab7840a49a4d9b6b
Billy Ceskavich <bceskavich@gmail.com>
2014-12-02 15:43:41 -0500

    More DB wrapper additions.

--- src/db.py
+++ src/db.py
@@ -1,13 +1,11 @@
+import json
+from bson.objectid import ObjectId
+
 from pymongo import MongoClient
 
 """
 TODO
 ----
-
-Init - setup connection to be used throughout class
-Setup - setup script username/password combos
-
-auth - login, logout
 
 get_project_list - list of project accounts, running collector count, description
 get_project_detail - list of running collectors & details
@@ -22,8 +20,10 @@ set_collector_status - collector at network level
 class DB(object):
 
     def __init__(self):
+        # Class instance connection to Mongo
         self.connection = MongoClient()
 
+        # App-wide config file for project info access
         self.config_db = self.connection.config
         self.stack_config = self.config_db.config
 
@@ -31,12 +31,26 @@ class DB(object):
         """
         Initial app-wide config setup for project_users
         Should pass a list of project_name, password, description params:
-        [{project_name: name, password: string, description: string}]
+        [{
+            project_name    : [project-name],
+            password        : [project-password],
+            description     : [project-description]
+        }]
         """
         for item in project_list:
-            self.stack_config.insert(item)
+            resp = self.auth(item['project_name'], item['password'])
+
+            if resp['status']:
+                print 'Project "%s" already exists!' % item['project_name']
+            else:
+                item['collectors'] = []
+                self.stack_config.insert(item)
 
     def auth(self, project_name, password):
+        """
+        Project auth function
+        """
+        # TODO - Flag issue that we know it's insecure & needs fix
         auth = self.stack_config.find_one({
             'project_name'  : project_name,
             'password'      : password})
@@ -48,15 +62,72 @@ class DB(object):
             status = 0
             project_id = None
 
-        return [status, project_id]
+        resp = {'status': status, 'project_id': project_id}
+
+        return resp
 
     def get_project_list(self):
-        pass
+        """
+        Generic function that return list of all projects in stack config DB
+        """
+        projects = self.stack_config.find()
+
+        if projects:
+            status = 1
+            project_count = self.stack_config.count()
+            project_list = [item for item in projects]
+            resp = {'status': status, 'project_count': project_count, 'project_list': project_list}
+        else:
+            status = 0
+            resp = {'status': status}
+
+        return resp
 
     def get_project_detail(self):
         pass
 
-    def set_collector_detail(self):
+    # TODO - enforce unique project names
+    def set_collector_detail(self, project_id, network, api, collector_name, api_credentials_dict, terms_list):
+        """
+        Sets up config collection for a project collector
+        """
+        resp = self.stack_config.find_one({'_id': ObjectId(project_id)})
+        project_name = resp['project_name']
+
+        terms = []
+        for term in terms_list:
+            terms.append({'term': term, 'collect': 1})
+
+        doc = {
+            'project_id'    : project_id,
+            'project_name'  : project_name,
+            'collector_name': collector_name,
+            'network'       : network,
+            'api'           : api,
+            'api_auth'      : api_credentials_dict,
+            'terms_list'    : terms,
+            'collector'     : {'active': 0, 'run': 0, 'collect': 0, 'update': 0},
+            'processor'     : {'active': 0, 'run': 0},
+            'inserter'      : {'active': 0, 'run': 0}
+        }
+
+        project_config_db = self.connection[project_name + 'Config']
+        coll = project_config_db.config
+
+        try:
+            coll.insert(doc)
+
+            resp = coll.find_one({'collector_name': collector_name})
+            collector_id = resp['_id']
+
+            self.stack_config.update({'_id': ObjectId(project_id)}, {'$push': {'collectors': {'name': collector_name, 'collector_id': collector_id, 'active': 0}}})
+            status = 1
+        except:
+            status = 0
+
+        return status
+
+    def update_collector_detail(self, collector_id, **kwargs):
         pass
 
     def set_network_status(self):
@@ -66,13 +137,16 @@ class DB(object):
         pass
 
 if __name__ == '__main__':
-    Test = DB()
+    projects = [
+        {
+            'project_name': 'ross',
+            'password': '1111',
+            'description': 'Test project three.'
+        }
+    ]
 
-    db = Test.connection.config
-    app_config = db.config
+    test_db = DB()
 
-    test = app_config.find_one({'module': 'processor'})
-    test_id = test['_id']
-    test = app_config.find_one({'_id': test_id})
-    print test
+    status = test_db.set_collector_detail('547e02e1eb8f8005abf243f2', 'twitter', 'track', 'govdata_track', {'application_key': '123'}, ['billy', 'ceskavich'])
+    print status
 
