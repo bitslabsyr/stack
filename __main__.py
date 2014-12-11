0e300696bf8b723306cc648ef29f8fa708c0e8e8
Billy Ceskavich <bceskavich@gmail.com>
2014-12-11 11:28:20 -0500

    Controller commands list not present.

--- __main__.py
+++ __main__.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     ]
 
     controller_processes = ['collect', 'process', 'insert']
+    controller_commands = ['start', 'stop', 'restart']
 
     try:
         wrapper = sys.argv[1]
