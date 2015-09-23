5cfe8e6d180d3df7466edc1e4e0bd6fafee0d757
Billy Ceskavich <bceskavich@gmail.com>
2015-09-23 15:49:40 -0400

    Basic email structure set up
    * Using Jinja2 templates to render dynamic content

--- scripts/notifier.py
+++ scripts/notifier.py
@@ -1,8 +1,18 @@
+from sendgrid import SendGridClient, Mail, SendGridError, SendGridClientError, SendGridServerError
+from jinja2 import Template
+
+# TODO - Set as os.environ var
+API_KEY = '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'
+
 def process_system_stats(system_stats):
     stats = {
+        'total': 0,
         'critical': None,
+        'total_critical': 0,
         'warn': None,
+        'total_warn': 0,
         'info': None,
+        'total_info': 0,
     }
 
     # A) Critical
@@ -21,14 +31,29 @@ def process_system_stats(system_stats):
 
     stats['critical'] = critical if critical else None
 
-    # TODO in future - warn & info
+    # TODO B & C - warn & info
+    warn = []
+    info = []
+
+    # D) Counts
+    stats['total_critical'] = len(critical)
+    stats['total_warn'] = len(warn)
+    stats['total_info'] = len(info)
+    stats['total'] = stats['total_critical'] + stats['total_warn'] + stats['total_info']
+
     return stats
 
 def process_project_stats(project_stats):
     stats = {
-        'critical': None,
-        'warn': None,
-        'info': None,
+        'collectors': {
+            'total': 0,
+            'critical': None,
+            'total_critical': 0,
+            'warn': None,
+            'total_warn': 0,
+            'info': None,
+            'total_info': 0,
+        }
     }
 
     # A) Critical
@@ -46,39 +71,101 @@ def process_project_stats(project_stats):
                 'message': 'Collector "%s" is running without a connection to the Streaming API' % collector['collector_name'],
             })
 
-    stats['critical'] = critical if critical else None
+    stats['collectors']['critical'] = critical if critical else None
+    stats['collectors']['total_critical'] = len(critical)
+    stats['collectors']['total'] = len(critical)
 
     # TODO in future - warn & info
     # TODO in future - stats for processor & inserter
+
     return stats
 
+def generate_email_text(report):
+    template = Template("""
+        <html>
+        <body>
+        <h1>STACKS System Status Update</h1>
+
+        <hr />
+
+        {% if report['system']['total'] %}
+            <h2>System Issues - {{ report['system']['total'] }}</h2>
+
+            {% if report['system']['critical'] %}
+            <h3>CRITICAL System Issues - {{ report['system']['total_critical'] }}</h3>
+            {% for issue in report['system']['critical'] %}
+                <p><strong>{{ issue['title'] }}</strong><br />{{ issue['message'] }}</p>
+            {% endfor %}
+            {% endif %}
+
+            {% if report['system']['warn'] %}
+            <h3>WARN System Issues - {{ report['system']['total_warn'] }}</h3>
+            {% for issue in report['system']['warn'] %}
+                <p><strong>{{ issue['title'] }}</strong><br />{{ issue['message'] }}</p>
+            {% endfor %}
+            {% endif %}
+
+            {% if report['system']['info'] %}
+            <h3>INFO System Issues - {{ report['system']['total_info'] }}</h3>
+            {% for issue in report['system']['info'] %}
+                <p><strong>{{ issue['title'] }}</strong><br />{{ issue['message'] }}</p>
+            {% endfor %}
+            {% endif %}
+        {% endif %}
+
+        {% if report['project']['collectors']['total'] %}
+            <h2>Collector Issues - {{ report['project']['collectors']['total'] }}</h2>
+
+            {% if report['project']['collectors']['critical'] %}
+            <h3>CRITICAL System Issues - {{ report['project']['collectors']['total_critical'] }}</h3>
+            {% for issue in report['project']['collectors']['critical'] %}
+                <p><strong>{{ issue['title'] }}</strong><br />{{ issue['message'] }}</p>
+            {% endfor %}
+            {% endif %}
+
+            {% if report['project']['collectors']['warn'] %}
+            <h3>WARN System Issues - {{ report['project']['collectors']['total_warn'] }}</h3>
+            {% for issue in report['project']['collectors']['warn'] %}
+                <p><strong>{{ issue['title'] }}</strong><br />{{ issue['message'] }}</p>
+            {% endfor %}
+            {% endif %}
+
+            {% if report['project']['collectors']['info'] %}
+            <h3>INFO System Issues - {{ report['project']['collectors']['total_info'] }}</h3>
+            {% for issue in report['project']['collectors']['info'] %}
+                <p><strong>{{ issue['title'] }}</strong><br />{{ issue['message'] }}</p>
+            {% endfor %}
+            {% endif %}
+        {% endif %}
+
+        </body>
+        </html>
+    """)
+
+    return template.render(report=report)
+
 def send_email(report):
-    print report
+    sg = SendGridClient(API_KEY, raise_errors=True)
+
+    message = Mail()
+    message.add_to('Billy Ceskavich <bceskavich@gmail.com>')
+    message.set_subject('STACKS Status Update')
+    message.set_html(generate_email_text(report))
+    message.set_from('STACKS <noreply@bits.ischool.syr.edu>')
+
+    try:
+        sg.send(message)
+    except SendGridError as e:
+        print e
+    except SendGridClientError as e:
+        print e
+    except SendGridServerError as e:
+        print e
 
 def process_and_notify(system_stats, project_stats):
-    report = {
-        'CRITICAL': {
-            'system': None,
-            'project': None,
-        },
-        'WARN': {
-            'system': None,
-            'project': None,
-        },
-        'INFO': {
-            'system': None,
-            'project': None,
-        },
-    }
-
-    system = process_system_stats(system_stats)
-    report['CRITICAL']['system'] = system['critical']
-    report['WARN']['system'] = system['warn']
-    report['INFO']['system'] = system['info']
-
-    project = process_project_stats(project_stats)
-    report['CRITICAL']['project'] = project['critical']
-    report['WARN']['project'] = project['warn']
-    report['INFO']['project'] = project['info']
+    # Empty report dict to be populated with stats
+    report = {}
+    report['system'] = process_system_stats(system_stats)
+    report['project'] = process_project_stats(project_stats)
 
     send_email(report) #, project_stats['email'])
