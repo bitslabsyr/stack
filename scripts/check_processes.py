4d7c915d875bbd18945c875088d0e2262c66bbb7
Billy Ceskavich <bceskavich@gmail.com>
2015-10-30 09:47:40 -0400

    Fixed process check method for notifier

--- scripts/check_processes.py
+++ scripts/check_processes.py
@@ -1,13 +1,18 @@
 import os
+import subprocess
+import re
 
 def pid_alive(pid):
     """
-    Uses OS Kill signal 0 to check if the process with given pid is running
-    Via - http://stackoverflow.com/questions/568271/how-to-check-if-there-exists-a-process-with-a-given-pid
+    Uses subprocess to check for active processes
+    http://stackoverflow.com/questions/7647167/check-if-a-process-is-running-in-python-in-linux-unix
     """
-    try:
-        os.kill(pid, 0)
-    except OSError:
+    ps = subprocess.Popen('ps -ef | grep {}'.format(pid), shell=True, stdout=subprocess.PIPE)
+    output = ps.stdout.read()
+    ps.stdout.close()
+    ps.wait()
+
+    if re.search(str(pid), output) is None:
         return False
     else:
         return True
@@ -21,8 +26,11 @@ def check_process_pid(pidfile):
         pf = file(pidfile, 'r')
         pid = int(pf.read().strip())
         pf.close()
+        print pid
         return pid_alive(pid)
-    except IOError:
+    except IOError as e:
+        print 'IOError on pidfile read'
+        print e
         return False
     except SystemExit:
         return False
