bb6692e928bde7cec4696055b16ea73a2d7ef842
Sikana Tanupabrungsun <Sikana@Sikanas-MacBook-Pro.local>
2017-10-16 15:36:06 -0400

    Revert "Update preprocess.py"
    This reverts commit 686244d255c0499eff97ade0eb24b0116ca3ff07.

--- app/twitter/preprocess.py
+++ app/twitter/preprocess.py
@@ -11,6 +11,7 @@
 
 import os.path
 import ConfigParser
+from pymongo import Connection
 import datetime
 import logging
 import logging.config
@@ -119,7 +120,6 @@ def go(project_id, rawdir, archdir, insertdir, logdir):
     project_name = project['project_name']
 
     configdb = project['project_config_db']
-    
     conn = db.connection[configdb]
     project_config_db = conn.config
 
